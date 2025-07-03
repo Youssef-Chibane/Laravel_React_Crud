f4d960e059bee47cd196ea7fc0c9239d12d01bae
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-07-03 17:51:49 +0100

    show latest posts first

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
     public function index()
     {
         return Inertia::render('posts/index', [
-            "posts" => Post::oldest()->paginate(5)
+            "posts" => Post::latest()->paginate(5)
         ]);
     }
     /**
